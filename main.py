84277c5325a2079a08673e245d377bc7c243cbcd
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-05-29 23:26:18 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,9 @@
+def ushlovchi_dastur(x):
+    if x > 0:
+        return x
+    else:
+        return 0
+
+# Test qilish
+print(ushlovchi_dastur(5))  # Chiqaradi: 5
+print(ushlovchi_dastur(-3))  # Chiqaradi: 0
